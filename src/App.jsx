8d8aff188ece86285a8e8183240f9e8a9bc4f60c
Abhisek7Acharya <madhabacharya734@gmail.com>
2025-05-14 06:33:07 +0545

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+
+import Register from "./pages/Register";
+import Signin from "./pages/Signin"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Home from "./pages/home";
+import Blog from "./pages/Blog";
+import Navbar from "./pages/Navbar";
+import { FaBeer } from 'react-icons/fa'
+import { MdHome } from 'react-icons/md'
+import { BsFillAlarmFill } from 'react-icons/bs'
+import SingleBlogpage from "./pages/SingleBlogpage";
+import CreateBlog from "./pages/create";
+import EditBlogs from "./pages/editblog";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+           <Route path="/" element={<Home />} /> 
+          <Route path="/register" element={<Register />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/home" element={<Home />} /> 
+          <Route path="/Blog" element={<Blog/>} />
+          {/* <Route path={`/singleblogs/${Blog._id}`} element={<Blog/>} /> */}
+         <Route path="/create" element={<CreateBlog/>} />
+        <Route path="/singleblogs/:id" element={<SingleBlogpage/>} />
+          <Route path="/Editblog" element={<EditBlogs/>} />
+          
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
